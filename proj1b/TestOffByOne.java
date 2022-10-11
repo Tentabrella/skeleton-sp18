5ead8ab7b8e132737d7a3693715cef48889517b3
sunnymilk <cduthushihong@gmail.com>
2022-10-11 15:15:34 +0800

    adjust proj1b fix style and testcases

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -8,11 +8,11 @@ public class TestOffByOne {
     public void testEqualChars() {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
-        assertFalse(offByOne.equalChars('a', 'e'));
-        assertFalse(offByOne.equalChars('a', 'z'));
-        assertFalse(offByOne.equalChars('a', 'a'));
-        assertFalse(offByOne.equalChars('a', 'A'));
         assertTrue(offByOne.equalChars('&', '%'));
-    }
 
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'z'));
+        assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('a', 'A'));
+    }
 }
