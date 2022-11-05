97479867913eed3f1689c930be0db08f1ce6513a
sunnymilk <cduthushihong@gmail.com>
2022-11-05 17:58:08 +0800

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,138 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import org.junit.Assert;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Percolation {
+    private final int N;
+    private int[][] grid;
+    private WeightedQuickUnionUF quickUnionUF;
+    private int openSites = 0;
+    private int[] directions = new int[]{-1, 0, 1, 0, -1};
+    private Set<Integer> fullParent;
 
+    /***
+     * create N-by-N grid, with all sites initially blocked
+     * @param N
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("the length of grid is " + N + " but is expected to be positive");
+        }
+        grid = new int[N][N];
+        this.N = N;
+        quickUnionUF = new WeightedQuickUnionUF(N * N);
+        fullParent = new HashSet<>();
+    }
+
+    /***
+     * open the site (row, col) if it is not open already
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+        if (grid[row][col] != 0) {
+            return;
+        }
+        grid[row][col] = 1;
+        for (int i = 0; i < 4; i++) {
+            int row2 = row + directions[i];
+            int col2 = col + directions[i + 1];
+            if (boundCheck(row2) && boundCheck(col2) && isOpen(row2, col2)) {
+                quickUnionUF.union(simpEncode(row, col), simpEncode(row2, col2));
+            }
+        }
+        int parent = quickUnionUF.find(simpEncode(row, col));
+        for (int i = 0; i < N; i++) {
+            if (grid[0][i] == 1 && parent == quickUnionUF.find(simpEncode(0, i))) {
+                fullParent.add(parent);
+            }
+        }
+        openSites++;
+    }
+
+    private int simpEncode(int row, int col) {
+        return row * N + col;
+    }
+
+    private int[] simpDecode(int n) {
+        int row = Math.floorDiv(n, N);
+        int col = n % N;
+        return new int[]{row, col};
+    }
+
+    /***
+     * is the site (row, col) open?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        return grid[row][col] != 0;
+    }
+
+    private boolean boundCheck(int n) {
+        if (n < 0 || n >= N) {
+            return false;
+        }
+        return true;
+    }
+
+    /***
+     * is the site (row, col) full?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+        if (grid[row][col] == 1) {
+            return fullParent.contains(quickUnionUF.find(simpEncode(row, col)));
+        }
+        return false;
+    }
+
+    /***
+     * number of open sites
+     * @return
+     */
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    /***
+     * does the system percolate?
+     * @return
+     */
+    public boolean percolates() {
+        for (int i = 0; i < N; i++) {
+            if (isFull(N - 1, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /***
+     * use for unit testing (not required)
+     * @param args
+     */
+    public static void main(String[] args) {
+        Percolation small = new Percolation(2);
+        Assert.assertFalse(small.percolates());
+        small.open(0, 0);
+        Assert.assertFalse(small.percolates());
+        small.open(0, 1);
+        Assert.assertEquals(2, small.numberOfOpenSites());
+        Assert.assertFalse(small.percolates());
+        small.open(1, 1);
+        Assert.assertTrue(small.percolates());
+        System.out.println("test pass");
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,81 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import edu.princeton.cs.introcs.Stopwatch;
 
+public class PercolationStats {
+    private final PercolationFactory factory;
+    private int T;
+    private int N;
+    private double[] record;
+
+    /***
+     * perform T independent experiments on an N-by-N grid
+     * @param N
+     * @param T
+     * @param pf
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.N = N;
+        this.T = T;
+        factory = pf;
+        record = new double[T];
+        helper();
+    }
+
+    /***
+     * sample mean of percolation threshold
+     * @return
+     */
+    public double mean() {
+        return StdStats.mean(record);
+    }
+
+    private void helper() {
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = factory.make(N);
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                percolation.open(row, col);
+            }
+            record[i] = percolation.numberOfOpenSites() / Math.pow(N, 2);
+        }
+    }
+
+    /***
+     * sample standard deviation of percolation threshold
+     * @return
+     */
+    public double stddev() {
+        return StdStats.stddev(record);
+    }
+
+    /***
+     * low endpoint of 95% confidence interval
+     * @return
+     */
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    /***
+     * high endpoint of 95% confidence interval
+     * @return
+     */
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public static void main(String[] args) {
+        int[] ns = new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        for (int n : ns) {
+            Stopwatch stopwatch = new Stopwatch();
+            PercolationStats ps = new PercolationStats(n, 100, new PercolationFactory());
+            double time = stopwatch.elapsedTime() * 1000;
+            System.out.printf("with N = %d run time is %f ms\n", n, time);
+            System.out.printf("range between %f and %f\n", ps.confidenceLow(), ps.confidenceHigh());
+        }
+    }
 }
